6ee49c950f3813debbfd19703727cd8baa867b85
jorgeortizc06 <jorgeortizc06@gmail.com>
2022-06-15 18:23:18 -0500

    Add library class-validator ad Dto

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,1 +1,13 @@
-export class CreateItemDto {}
+import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
+
+export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsOptional()
+  @IsString()
+  description: string;
+  @IsInt()
+  @Min(0)
+  quantity: number;
+}
